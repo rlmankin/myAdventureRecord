8cfde0a70d8dc044e2e46ed83adccdd86241570e
rlm <rlmankin@me.com>
2021-02-03 15:39:35 -0700

    Initial Commit

---
+++ myAdventureRecord/myAdventureRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAdventureRecordApp.swift
+//  myAdventureRecord
+//
+//  Created by Robb Mankin on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct myAdventureRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
