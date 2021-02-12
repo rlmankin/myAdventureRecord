31868c52d27b9ea567b34df46e7cf6736775c864
rlm <rlmankin@me.com>
2021-02-12 11:02:53 -0700

    Interim commit.  Rewrote nested ForEach sequence (url/track) to create a flat track array to eliminate weird nested ForEach  behavior with respect to .tabItem naming

--- myAdventureRecord/GpxParser/GpxParseView.swift
+++ myAdventureRecord/GpxParser/GpxParseView.swift
@@ -13,59 +13,37 @@ struct GPXParsingView: View {
 	@State private var tabcount : Int = 1
 	@State private var beenParsed: Bool = false
 	
-	
+	func parseAll(urlArray: [URL]) -> [Track] {
+		var trackArray : [Track] = []
+		for url in urlArray {
+			let parseGPX = parseGPXXML()
+			let parseSuccess = parseGPX.parseURL(gpxURL: url)
+			if parseSuccess {
+				for  track in (0 ... parseGPX.allTracks.count - 1) {
+					trackArray.append(parseGPX.allTracks[track])
+				}
+			}
+		}
+		return trackArray
+	}
 	
 	var body: some View {
 		
 		if !selectedURLs.isEmpty {
-			TabView(selection: $selectedTab) {
-				
-				ForEach (0 ... selectedURLs.count - 1, id:\.self) { url in
-					
-					let parseGPX = parseGPXXML()
-					let parseSuccess = parseGPX.parseURL(gpxURL: selectedURLs[url])
-					if parseSuccess {
-						ForEach (0 ... parseGPX.allTracks.count - 1, id:\.self) { track in
-							let header = parseGPX.allTracks[track].header
-							let tagValue = Int(url*10 + track)
-							
-							//let x: String = String(String(url) + ", " 	+ String(track) + ", ") //+ String(selectedTab))
-							//let y = x //+// parseGPX.allTracks[track].print(true)
-							Text( parseGPX.allTracks[track].print(true))
-								.font(.footnote)
-								.tag(tagValue)
-								.tabItem { Text("\(tagValue)") //", \(header)")
-									.font(.caption)
-								}.onAppear {
-									print("Text.onAppear - \(tagValue), | \(parseGPX.allTracks[track].header)")
-								}.onDisappear {
-									print("Text.onDisappear - \(url),\(track), | \(parseGPX.allTracks[track].header)")
-								}
-						}.onAppear {
-							beenParsed.toggle()
-							tabcount += 1
-							//selectedTab = tabcount
-							print("ForEachTrack.onAppear Called : \(url)\n")
-							print("---------------------------------------")
-						}
-						.onDisappear {
-							tabcount -= 1
-							//selectedTab = tabcount
-							print("ForEachTrack.onDisappear Called : \(url)")
-						}
-					} else {
-						Text("Parse of \(selectedURLs[url].absoluteURL) failed")
-					}
-					
-					
-					//tabcount += 1
-					// this is where I will put the actual parse
-				}.onAppear {
-					print("ForeEachURL.onAppear called : selectedTab - \(selectedTab)")
-				}.onDisappear {
-					print("ForeEachURL.onDisappear called : selectedTab - \(selectedTab)")
-				}
 			
+			let parsedTracks = parseAll(urlArray: selectedURLs)
+			var trackcount : Int = 0
+			
+			TabView(selection: $selectedTab) {
+				ForEach (0 ... parsedTracks.count - 1, id:\.self)
+					{ track in
+							let header = parsedTracks[track].header
+							Text( parsedTracks[track].print(true))
+								.font(.footnote)
+								.tag(track)
+								.tabItem { Text("\(parsedTracks[track].header)")}
+									.font(.caption)
+					}
 			}
 		} else {
 			Text("")
